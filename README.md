0d06e9707fb7be166a67401e924bcd5246639d2f
jole84 <johanlennartsson84@gmail.com>
2026-08-12 20:45:35 +0200

    changes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple webapp
+
+Jole84 Arch Linux customizations
+
+    curl https://raw.githubusercontent.com/jole84/arch-jole84/refs/heads/main/customizations.sh | fish
+    curl -O https://raw.githubusercontent.com/jole84/arch-jole84/refs/heads/main/easypacman
--- customizations.sh
+++ customizations.sh
@@ -1,7 +1,5 @@
 #!/bin/fish
 
-# curl https://raw.githubusercontent.com/jole84/arch-jole84/refs/heads/main/customizations.sh | fish
-
 # pacman
     # -e 's/#VerbosePkgLists/VerbosePkgLists/' \
 sudo sed \
@@ -9,18 +7,6 @@ sudo sed \
     -e 's/ParallelDownloads = 5/ParallelDownloads = 10/' \
     -i /etc/pacman.conf
 
-# fish alias
-alias pacinstall='sudo pacman -S' -s # install package
-alias pacremove='sudo pacman -Rns' -s # remove package
-alias pacupdate='sudo pacman -Syu && flatpak update' -s
-# alias pacupdate='sudo pacman -Syu && flatpak update && find ~/aur -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;' -s # update
-alias paccheckupdate='sudo pacman -Sy >/dev/null && pacman -Qu' -s # check for updates
-alias pacsearch='pacman -Ss' -s # search
-alias pacinfo='pacman -Si' -s # show package info
-alias paclistinstalled='pacman -Qe' -s # list explicitly installed packages
-alias paclistorphans='pacman -Qdt' -s # list explicitly installed uneeded packages
-alias paclistaliens='pacman -Qem' -s # list explicitly installed packages not in sync database
-alias pacautoremove='sudo pacman -Qdtq | sudo pacman -Rsu -' -s # autoremove unneeded
 
 # fish settings
 set -U fish_greeting
